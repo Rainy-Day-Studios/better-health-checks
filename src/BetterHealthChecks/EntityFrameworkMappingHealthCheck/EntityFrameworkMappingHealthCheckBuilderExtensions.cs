a7d4f2f5635a5fc09aa1cfb1af1fd3a287656dff
Isaac Smith <isaac.smith.sf@gmail.com>
2022-05-15 10:14:55 -0500

    Added XML comments for public members

--- src/BetterHealthChecks/EntityFrameworkMappingHealthCheck/EntityFrameworkMappingHealthCheckBuilderExtensions.cs
+++ src/BetterHealthChecks/EntityFrameworkMappingHealthCheck/EntityFrameworkMappingHealthCheckBuilderExtensions.cs
@@ -9,12 +9,21 @@ public static class EntityFrameworkMappingHealtCheckhBuilderExtensions
 {
     private const string DEFAULT_NAME = "Entity Framework Mappings";
 
+    /// <summary>
+    /// Registers an EntityFrameworkMappingHealthCheck for the provided TDbContext type. This health check 
+    /// will validate mappings between entity framework models and their underlying tables.
+    /// </summary>
     public static IHealthChecksBuilder AddEntityFrameworkMappingCheck<TDbContext>(
         this IHealthChecksBuilder builder) where TDbContext : DbContext
     {
         return AddEntityFrameworkMappingCheck<TDbContext>(builder, (opts) => { });
     }
 
+    /// <summary>
+    /// Registers an entity framework mapping health check for the provided TDbContext type. This health check 
+    /// will validate the mappings between entity framework models and their underlying tables.
+    /// </summary>
+    /// <param name="optionsBuilder">Optional configuration action for the health check.</param>
     public static IHealthChecksBuilder AddEntityFrameworkMappingCheck<TDbContext>(
         this IHealthChecksBuilder builder,
         Action<EntityFrameworkMappingHealthCheckOptions> optionsBuilder) where TDbContext : DbContext
--- src/BetterHealthChecks/EntityFrameworkMappingHealthCheck/EntityFrameworkMappingHealthCheckOptions.cs
+++ src/BetterHealthChecks/EntityFrameworkMappingHealthCheck/EntityFrameworkMappingHealthCheckOptions.cs
@@ -2,6 +2,10 @@ namespace BetterHealthChecks.EntityFrameworkMappingHealthCheck;
 
 public class EntityFrameworkMappingHealthCheckOptions
 {
+    /// <summary>
+    /// Configures the health check name. If not provided, a default will be used.
+    /// </summary>
     public string HealthCheckName { get; set; }
+
     public IEnumerable<string> Tags { get; set; }
 }
